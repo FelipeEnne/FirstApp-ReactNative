c5345156b0da8fb556fbdc0d8e634d5c39732847
FelipeEnne <felipeenne@gmail.com>
2020-11-02 18:51:47 -0300

    style props

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
+
+const Textt = styled.Text`
+  color:${props=>props.color};
+  font-size:15px;
+`;
 
 const style = StyleSheet.create({
   page:{
@@ -18,7 +24,9 @@ const TextoInit = () => {
     <SafeAreaView style={style.page}>
       <Text style={{color:'white', fontSize:25}}>Hello</Text>
       <Text style={style.text}>Hello</Text>
-      <Text style={[style.text, {fontSize:17}]}>Hello</Text>
+      <Textt color="white">Hello</Textt>
+      <Textt color="red">Hello</Textt>
+      <Textt color="black">Hello</Textt>
     </SafeAreaView>
   )
 }
