8870522cbca576c8b67479d27ddb2accbff737c6
FelipeEnne <felipeenne@gmail.com>
2020-11-02 18:02:24 -0300

    styling

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Text } from 'react-native'
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+const style = StyleSheet.create({
+  page:{
+    width:200,
+    height:200,
+    backgroundColor:'red'
+  },
+  text:{
+    color:'white'
+  }
+});
 
 
 const TextoInit = () => {
   return (
-    <Text>Hello</Text>
+    <SafeAreaView style={style.page}>
+      <Text style={{color:'white', fontSize:25}}>Hello</Text>
+      <Text style={style.text}>Hello</Text>
+      <Text style={[style.text, {fontSize:17}]}>Hello</Text>
+    </SafeAreaView>
   )
 }
 
